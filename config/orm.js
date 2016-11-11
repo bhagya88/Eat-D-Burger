3d09d944cddda46377eec2d60e3dfb05e76ad6a0
bhagya <bhagya@outlook.com>
2016-11-10 21:29:43 -0600

    added orm - selectAll , insertOne and updateOne

---
+++ config/orm.js
@@ -0,0 +1,29 @@
+var connection = require('./connection.js');
+
+// object relational mapper (ORM)
+
+var orm = {
+
+	// selects all rows
+	selectAll: function (table,cb) {
+		var queryString = 'SELECT * FROM ' + table ;
+		connection.query(queryString, cb);
+	},
+
+	// inserts one row
+	insertOne: function (table,row,cb) {
+		var queryString = 'INSERT INTO ' + table + ' SET ?';
+		connection.query(queryString,row,cb);
+	
+	},
+
+	// updates one row
+	updateOne: function (table,columns,where,cb) {
+		var queryString = 'UPDATE ' + table + ' SET ? WHERE ?';
+		connection.query(queryString,[columns,where],cb);
+		
+	}
+};
+
+
+module.exports = orm;
